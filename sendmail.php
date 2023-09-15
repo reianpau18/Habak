546d7700edbf8342a343a1356d9b9f7cafe13ae4
reianpau18 <138023550+reianpau18@users.noreply.github.com>
2023-09-16 05:01:05 +0800

    recaptcha 2

--- sendmail.php
+++ sendmail.php
@@ -45,14 +45,13 @@ if ($message_field !== "") {
         $item = '<tr ' . $background . ' align="center" style="border-bottom: 1px solid #eeeeee;">';
         $item .= '<td width="33%" style="border-right: 1px solid #eeeeee;"><p style="line-height: 52px; margin: 0; font-size: 12px; color: #363636;">' . $product->title . '</p></td>';
         $item .= '<td width="33%" style="border-right: 1px solid #eeeeee;"><p style="line-height: 52px; margin: 0; font-size: 12px; color: #363636;">' . $product->code . '</p></td>';
-        $item .= '<td width="33%"><p style="line-height: 52px; margin: 0; font-size: 12px; color: #363636;">' . $product->value . '</p></td>';
+        $item .= '<td width="33%"><p style="line-height: 52px; margin: 0; font-size: 12px; color: #363636;">' . $product->price . '</p></td>';
         $item .= '</tr>';
 
         $tableRows .= $item;
     }
 
     $message = str_replace('{{ prodList }}', $tableRows, $message);
-    $message = str_replace('{{ prodpriceList }}', $tableRows, $message);
 }
 
 $headers = "MIME-Version: 1.0\r\n";
